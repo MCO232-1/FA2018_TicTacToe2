0ca4aa973e9d25327b14e42db9780a8a973d12bc
J Robinson <jrobinson@touro.edu>
2018-03-12 17:46:40 -0400

    .

---
+++ src/tictactoe2/TicTacToe.java
@@ -0,0 +1,80 @@
+package tictactoe2;
+
+import java.util.Scanner;
+
+enum Cell {
+    NONE, O, X
+};
+
+class TicTacToe {
+
+    private boolean isPlayerOneMove;
+    private Cell[][] grid;
+
+    TicTacToe(int playerCount) {
+        grid = new Cell[3][3];
+        for (int row = 0; row < 3; row++) {
+            for (int col = 0; col < 3; col++) {
+                grid[row][col] = Cell.NONE;
+            }
+        }
+        // isPlayerOneMove = true;
+        // TODO implementb one player game
+    }
+
+    public void displayBoard() {
+
+        for (int row = 0; row < 3; row++) {
+            for (int col = 0; col < 3; col++) {
+                System.out.print(convertToString(grid[row][col]));
+            }
+            System.out.println();
+        }
+    }
+
+    public void promptAndMakeMove() {
+        isPlayerOneMove = !isPlayerOneMove; // toggle
+
+        Scanner kb = new Scanner(System.in);
+        System.out.println("Enter your Move (A1 - C3, Mr." + (isPlayerOneMove ? "X" : "O"));
+
+        String userMove = kb.next();
+        int col = userMove.charAt(0) - 'A';
+        int row = userMove.charAt(1) - '1';
+
+        if (grid[row][col] == Cell.NONE) {
+            grid[row][col] = isPlayerOneMove ? Cell.X : Cell.O;
+        }
+
+        //TODO make safe ..add validation A-C, 1-3
+    }
+
+    boolean gameOver() { 
+        // diag #1
+        if (grid[0][0] != Cell.NONE
+                && grid[0][0] == grid[1][1]
+                && grid[2][2] == grid[1][1]) {
+            return true;
+        }
+
+        //TO diag2 , rows, cols, no legal moves
+        return false;
+    }
+
+    Cell getWinner() {        
+        if (gameOver())
+            return  isPlayerOneMove ? Cell.X : Cell.O;
+        return Cell.NONE;
+     }
+
+    private String convertToString(Cell cell) {
+        switch(cell){
+            case NONE: return " ";
+            case O: return "O";
+            case X: return "X";
+            default:
+                throw new RuntimeException(cell.toString());
+        }
+    }
+
+}
